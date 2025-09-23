d7185963130e6ec44713e5827507ad49a3a7d161
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-09-23 12:21:14 +0200

    edycja literowek - lekkie poprawki wygladu

--- 2025_09_10_egzamin_gra_kosci/app/src/main/java/edu/zsk/a2025_09_10/MainActivity.java
+++ 2025_09_10_egzamin_gra_kosci/app/src/main/java/edu/zsk/a2025_09_10/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Home");
+        setTitle("Kosci");
 
 
         diceViews = new ImageView[]{
