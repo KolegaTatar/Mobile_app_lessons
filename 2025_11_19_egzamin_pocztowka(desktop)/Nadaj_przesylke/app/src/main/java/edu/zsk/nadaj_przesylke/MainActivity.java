daf3fd1d78e012cbf22762363f16e130e0e78371
Wiktor <wiktor.tatarynowicz@gmail.com>
2025-11-25 17:33:48 +0100

    dodanie plikow z lekcji 19.11.2025

--- 2025_11_19_egzamin_pocztowka(desktop)/Nadaj_przesylke/app/src/main/java/edu/zsk/nadaj_przesylke/MainActivity.java
+++ 2025_11_19_egzamin_pocztowka(desktop)/Nadaj_przesylke/app/src/main/java/edu/zsk/nadaj_przesylke/MainActivity.java
@@ -1,6 +1,13 @@
 package edu.zsk.nadaj_przesylke;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +17,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_zatwierdz, btn_cena;
+    private EditText miasto, kod_pocztowy, nr_ulicy;
+    private TextView cenaText;
+    private ImageView obrazView;
+    private RadioGroup radio_group;
+    private RadioButton pocztowkaRadio, listRadio, paczkaRadio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,60 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn_zatwierdz = findViewById(R.id.btn_zatwierdz);
+        btn_cena = findViewById(R.id.btn_cena);
+
+        miasto = findViewById(R.id.miasto);
+        kod_pocztowy = findViewById(R.id.kod_pocztowy);
+        nr_ulicy = findViewById(R.id.nr_ulicy);
+
+        obrazView = findViewById(R.id.zdjecie);
+        cenaText = findViewById(R.id.cena_text);
+
+        radio_group = findViewById(R.id.radio_group);
+        pocztowkaRadio = findViewById(R.id.pocztowka);
+        listRadio = findViewById(R.id.list);
+        paczkaRadio = findViewById(R.id.paczka);
+
+        pocztowkaRadio.setChecked(true);
+        obrazView.setImageResource(R.drawable.pocztowka);
+        cenaText.setText("Cena: 1 zł");
+
+        btn_zatwierdz.setOnClickListener(v->Zatwierdz());
+        btn_cena.setOnClickListener(v->Sprawdz_cene());
     }
+    protected void Sprawdz_cene(){
+        if(pocztowkaRadio.isChecked()){
+            obrazView.setImageResource(R.drawable.pocztowka);
+            cenaText.setText("Cena: 1 zł");
+        }
+        else if(listRadio.isChecked()){
+            obrazView.setImageResource(R.drawable.list);
+            cenaText.setText("Cena: 1,5 zł");
+        }
+        else if(paczkaRadio.isChecked()){
+            obrazView.setImageResource(R.drawable.paczka);
+            cenaText.setText("Cena: 10 zł");
+        }
+    }
+
+    protected void Zatwierdz(){
+        String KodPocztowy = kod_pocztowy.getText().toString().trim();
+
+        try {
+            Integer.parseInt(KodPocztowy);
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Kod pocztowy powinien się składać z samych cyfr", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(KodPocztowy.length()!=5){
+            Toast.makeText(this,"Nieprawidłowa liczba cyfr w kodzie pocztowym",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Toast.makeText(this, "Dane przesyłki zostały wprowadzone",Toast.LENGTH_SHORT).show();
+
+    }
+
 }
